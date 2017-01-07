f779d2de32c90e9eea9979d216fd615dd422f21e
alexanderwe <weiss_alex@gmx.net>
2017-01-07 02:05:38 +0100

    Updated dependencies, file structure change, version change.

--- client/Routes.jsx
+++ client/Routes.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {mount} from 'react-mounter';
 
 import {MainLayout} from './layouts/MainLayout.jsx';
-import HelloWorld from './HelloWorld.jsx';
+import HelloWorld from './components/HelloWorld.jsx';
 
 FlowRouter.route('/', {
     action() {
